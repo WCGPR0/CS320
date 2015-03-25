97fee93ae03ce7ecf4580554fc7582a7c2b8c152
Victor Wu <vwu4@cs.binghamton.edu>
2015-03-25 04:34:19 -0400

    Updated project but realized crucial concept misunderstood: each element in a table has a state, not just the entity as a whole

--- prj1/wu_prj1.cpp
+++ prj1/wu_prj1.cpp
@@ -14,7 +14,7 @@ using namespace std;
 /// Bimodal  Predictor with a single bit of history
 /** \param tableSize integer that is the size of the Table */
 int bimodalOne(vector<bool> &table, int bitSize, unsigned long long &myLine, string &myAction, int &count) {
-int index = (int) myLine & 0x1 << bitSize;
+int index = (int) (0x1 << bitSize) & myLine;
 //Compares Table Taken with Action Taken
 if ((table[index] == 0) != (myAction == "T"))
 	++count;
@@ -24,7 +24,7 @@ return 0;
 /// Bimodal  Predictor with a two bit of history
 /** \param tableSize integer that is the size of the Table */
 int bimodalTwo(vector<bool> &table, int bitSize, unsigned long long &myLine, string &myAction, bool &myFlag, int &count) {
-int index = (int) myLine & 0x1 << bitSize;
+int index = (int) (0x1 << bitSize) & myLine;
 //Compares Table Taken with Action Taken
 if (table[index] == 1) {	
 	if (myAction == "NT") { //Transition from State TT to T in State Machine
@@ -46,71 +46,43 @@ else {
          ++count;
       }
    }	
-	else if (!myFlag) myFlag = !myFlag; //Transition from State N to NN
-	}	
+	else if (!myFlag) myFlag = !myFlag; //Transition from State N to NN	
 }
 return 0;
 } 
 
 
 /// GShare Predictor
-int gShare(vector<bool> &table, unsigned long long &myLine, string &myAction, int &globalHistory, bool &myFlag, int &count) {
-int index = (int) globalHistory ^ myLine;
-if (table[index] == 1) {
-   if (myAction == "NT") { //Transition from State TT to T in State Machine
-      if (myFlag)
-         myFlag = false;
-      else { //Transition from State T to N in State Machine
-         table[index] = 0;
-         ++count;
-      }
-   }
-   else if (!myFlag) myFlag = !myFlag;
+int gShare(vector<bool> &table, int bitSize, unsigned long long &myLine, string &myAction, unsigned int &globalHistory, int &historySize, bool &myFlag, int &count) {
+unsigned int index = (unsigned int) globalHistory << (bitSize - historySize++) ^ (myLine & ((1 << bitSize) - 1));
+
+bool action = (myAction == "T") ? 1 : 0;
+if (action != table[index]) {
+if (!myFlag) { table[index] = !table[index]; ++count; } //Transition from State T to N or N to T
+else { myFlag = !myFlag; } //Transition from State TT to T or NN to N
 }
-else {
-     if (myAction == "T") { //Transition from State NN to N in State Machine
-      if (myFlag)
-         myFlag = false;
-      else { //Transition from State N to T in State Machine
-         table[index] = 1;
-         ++count;
-      }
-   }
-      else if (!myFlag) myFlag = !myFlag; //Transition from State T to TT
-   }
-   //State 
-&& (myAction == "NT")) || ((table[index] == 1) && (myAction == "T"))) {
-	table[index] = !table[index];
-	++count;
+else if (!myFlag) myFlag = !myFlag; //Transition from State T to TT or N to NN
+globalHistory <<= 1;
+globalHistory |= action;
+globalHistory &= (1 << historySize)-1; 
+
+return table[index] | (myFlag << 1);
 }
-return index;
-}
+
 /*
 /// Tournament Predictor
-int tournamentPredictor(bool *table, int bitSize, unsigned long long &myLine, string &myAction, bool *myFlag, int *count, int T1, int T2) {
-int index = (int) myLine & 0x1 << bitSize;
-if (table[index] == 0) {
-//State PG in State Machine
-	if ((*myFlag == true) && ((T1 == 0) && (T2 != 0)))
-			*myFlag = !(*myFlag);
-//State G in State Machine
-	else if (*myFlag == false) {
-		if ((T1 == 0) && (T2 != 0)) {
-		*count++;
-		
-		}
+int tournamentPredictor(vector<bool> &table, int bitSize, unsigned long long &myLine, string &myAction, bool &myFlag, int &count, int T1, int T2) {
+int index = (int) (0x1 << bitSize) & myLine;
+
+if (T2 != T1) {
+	if (!myFlag) { 
+		if (T2) table[index] = 1;
+		else table[index] = 0;
+		++count; } 
+	else { myFlag = !myFlag; } //Transition from State TT to T or NN to N
 	}
-}
-else {
-//State BB in State Machine
-	if ((*myFlag == true) && ((T1 == 1) && (T2 != 1)))
-			*myFlag = !(*myFlag);
-//State BG in State Machine
-	else if (*myFlag == false) {
-		if ((T1 == 1) && (T2 != 1))
-		*count++;
-	}	
-}
+else if (!myFlag) myFlag = !myFlag; //Transition from State T to TT or N to NN
+
 return 0;
 }
 */
@@ -123,10 +95,10 @@ else {
 ifstream myFile(argv[1]);
 unsigned long long myLine = 0;
 string myAction, line;
-int branches = 0, always_taken_count = 0, not_taken_count = 0, bimodalOne_count[SIZES] = {0}, bimodalTwo_count[SIZES] = {0}, global_count = 0, tournament_count = 0;
+int branches = 0, always_taken_count = 0, not_taken_count = 0, bimodalOne_count[SIZES] = {0}, bimodalTwo_count[SIZES] = {0}, global_count = 0, tournament_count = 0, historyCount = 3;
 vector<bool> bimodalOneTable[SIZES], bimodalTwoTable[SIZES], gShareTable;
 
-register int globalHistory = 0;
+register unsigned int globalHistory = 0;
 bool gFlag = 0, tournamentFlag = 0;
 // \brief strong flags
 bool bimodalTwo_flags[SIZES];
@@ -156,13 +128,13 @@ if (myFile.is_open()) {
 			bimodalOne(bimodalOneTable[i], array[i],  myLine, myAction, bimodalOne_count[i]);
 			bimodalTwo(bimodalTwoTable[i], array[i],  myLine, myAction, bimodalTwo_flags[i], bimodalTwo_count[i]);
 		}	*/
-		T2 = gShare(gShareTable, myLine, myAction, globalHistory, gFlag, global_count);
+		T2 = gShare(gShareTable,array[SIZES], myLine, myAction, globalHistory, historyCount, gFlag, global_count);
 		//tournamentPredictor(tournamentTable, myLine, myAction, &tournament_count, T1, T2);
 		++branches;
 	}
 
 //Output
-cout << always_taken_count << ',' << branches << ';' << endl;
+/*cout << always_taken_count << ',' << branches << ';' << endl;
 cout << not_taken_count << ',' << branches << ';' << endl;
 for (int i = 0; i < SIZES; i++)
 	cout << bimodalOne_count[i] << ',' << branches << ';';
@@ -171,7 +143,8 @@ for (int i = 0; i < SIZES; i++)
 	cout << bimodalTwo_count[i] << ',' << branches << ';';
 	cout << endl;
 	myFile.close();
-	
+*/
+cout << global_count << ',' << branches << ';' << endl; 
 }
 else {
 cerr << "Unable to open input file" << endl;
